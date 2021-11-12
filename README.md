44f4495ae81a3fe8282f5579eb9ede39dbaaf527
Jon Christie <jonpchristie@gmail.com>
2021-11-12 14:00:19 -0500

    videos

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # THREEJS
 An increasing collection of graphics and animations created using three.js.
+
+## CUBE, SPHERES, TEXTURE MAPPING, animations
+
+
+<video width="320" height="240" controls>
+  <source src="./videos/2DONUTS.mov" type="mov">
+</video>
+
+# This repo has a video on its readme :p
+
+
+https://user-images.githubusercontent.com/58986949/115314310-805b2780-a1a7-11eb-8558-648a367ea231.mp4
+
--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'; 
-// J SCENE
+
 const scene = new THREE.Scene();
-// J CAMERA
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-// J RENDERER
+
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
-// J METHODS FOR CAM + REND
+
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-// J renderer.render CALLED HERE AND @ END OF LOOP
 renderer.render( scene, camera );
 
-// J LIGHTS + CONTROLS
+// J TORUS
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshStandardMaterial( { color: 0xffffff });// no light source required 
+const torus = new THREE.Mesh( geometry, material ); 
+
+scene.add(torus)
+
+// J TORUS 2
+// const geometry2 = new THREE.TorusGeometry( 8, 3, 16, 100 );
+// const material2 = new THREE.MeshStandardMaterial( { color: 0x999999 });// no light source required 
+// const torus2 = new THREE.Mesh( geometry2, material2 ); 
+
+// scene.add(torus2)
+
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(5,5,5)
 
@@ -26,85 +38,33 @@ scene.add( pointLight, ambientLight )
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// J TORUS
-// const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-// const material = new THREE.MeshStandardMaterial( { color: 0xffffff });
-// const torus = new THREE.Mesh( geometry, material ); 
-// scene.add(torus)
 
-// J TORUS 2
-// const geometry2 = new THREE.TorusGeometry( 8, 3, 16, 100 );
-// const material2 = new THREE.MeshStandardMaterial( { color: 0x999999 });// no light source required 
-// const torus2 = new THREE.Mesh( geometry2, material2 ); 
-// scene.add(torus2)
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
+  const star = new THREE.Mesh( geometry, material );
 
-// J STAR LOOP
-// function addStar() {
-//   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-//   const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
-//   const star = new THREE.Mesh( geometry, material );
-//   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
-//   star.position.set(x, y, z);
-//   scene.add(star)
-// }
-// Array(1000).fill().forEach(addStar)
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
 
-// const spaceTexture = new THREE.TextureLoader();
-// scene.background = spaceTexture;
+  star.position.set(x, y, z);
+  scene.add(star)
+}
+
+Array(1000).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader();
+scene.background = spaceTexture;
 // const spaceTexture = new THREE.TextureLoader().load('./images/FS.jpg');
 // scene.background = spaceTexture;
-
-document.body.onscroll = moveCamera
-
-// J  PICTURED BOX
-const jonTexture = new THREE.TextureLoader().load('./images/jCircle.png');
-const jon = new THREE.Mesh(
-  new THREE.BoxGeometry(3,3,3),
-  new THREE.MeshBasicMaterial( {map: jonTexture })
-)
-scene.add(jon);
-
-// J MOON
-const moonTexture = new THREE.TextureLoader().load('./images/jCircle.png');
-// const normalTexture = new THREE.TextureLoader().load('./images/jCircle.png');
-
-const moon = new THREE.Mesh(
-new THREE.SphereGeometry(3,32,32),
-new THREE.MeshStandardMaterial( {
-  map: moonTexture, 
-  // normalMap: normalTexture
-})
-);
-
-scene.add(moon)
-
-// moon.position.z = 30;
-// moon.position.setX(-10);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  jon.position.x = 7;
-  jon.rotation.x += 0.015;
-  jon.rotation.y += 0.015;
-  jon.rotation.z += 0.015;
-
-  moon.rotation.x += 0.015;
-  moon.rotation.y += 0.015;
-  moon.rotation.z += 0.015;
-
-  // jon.rotation.y += 0.01;
-  // jon.rotation.z += 0.01;
-
-  // camera.position.z = t * -0.01; 
-  // camera.position.x  = t * -0.002; 
-  // camera.position.y  = t * -0.002; 
-
-  // torus.rotation.x += 0.07;
-  // torus.rotation.y += 0.005;
-  // torus.rotation.z += 0.015;
-  // torus.translateY(.03);
-  // torus.translateX(-.03);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+  torus.translateY(.03);
+  torus.translateX(-.03);
   // torus2.rotation.x += 0.03;
   // torus2.rotation.y += 0.005;
   // torus2.rotation.z += -0.01;
@@ -116,9 +76,22 @@ function animate() {
 
 animate()
 
-function moveCamera() {
-  // call where the user is currently scrolled to:
-  // gives us the dimensions of the viewport and the top property will shows us exactly how far we are from on top of the webpage.
-  const t = document.body.getBoundingClientRect().top;
-  // now we can start making changes to properties on our 3D objects
-}
+// const jonTexture = new THREE.TextureLoader().load('./images/jCircle.png');
+
+// const jon = new THREE.Mesh(
+//   new THREE.BoxGeometry(3,3,3),
+//   new THREE.MeshBasicMaterial( {map: jonTexture })
+// )
+
+// scene.add(jon);
+
+// const moonTexture = new THREE.TextureLoader().load('./images/moon.jpg');
+
+// const moon = new THREE.Mesh(
+// new THREE.SphereGeometry(3,32,32),
+// new THREE.MeshBasicMaterial( {
+//   map: moonTexture, 
+// })
+// );
+
+// scene.add(moon)
